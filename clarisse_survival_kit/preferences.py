9654ec9a6e880bd8857d568f4326cde9d8b35226
til <tilstrobl@movingmedia.de>
2020-06-08 18:25:32 +0200

    rewritten preferences gui to be crash-proven as described by Yanik

--- clarisse_survival_kit/preferences.py
+++ clarisse_survival_kit/preferences.py
@@ -19,122 +19,131 @@ def preferences_list():
     return preferences
 
 
-def preferences_gui(**kwargs):
-    ix = get_ix(kwargs.get('ix'))
-    preferences = preferences_list()
+# class TerrainGui(ix.api.GuiWindow):
+#     def __init__(self, title, x, y, w, h):
+#         super(TerrainGui, self).__init__(ix.application.get_event_window(), x, y, w, h)
+#         self.set_title(title)
+#         self.panel = ix.api.GuiPanel(self, 0, 0, self.get_width(), self.get_height())
+#         self.panel.set_constraints(ix.api.GuiWidget.CONSTRAINT_LEFT, ix.api.GuiWidget.CONSTRAINT_TOP,
+#                                    ix.api.GuiWidget.CONSTRAINT_RIGHT, ix.api.GuiWidget.CONSTRAINT_BOTTOM)
+#         self.check_box = ix.api.GuiCheckbox(self.panel, 140, 130, "test")
+#         self.check_box.set_value(True)
+#         self.connect(self.check_box, "EVT_ID_CHECKBOX_CLICK", self.on_event)
+#
+#     def on_event(self, sender, evtid):
+#         print("event received", sender, evtid)
+#
+# window = TerrainGui("Heightmap wizard", 900, 450, 400, 640)
+# window.show()
+# while window.is_shown():
+#     ix.application.check_for_events()
+# window.destroy()
 
-    class EventRewire(ix.api.EventObject):
-        def ok(self, sender, evtid):
-            self.apply(sender, evtid)
-            sender.get_window().hide()
 
-        def apply(self, sender, evtid):
-            for pref_id in range(len(preferences)):
-                key = preferences[pref_id]['key']
-                if isinstance(input_fields[pref_id], ix.api.GuiCheckbox):
-                    value = input_fields[pref_id].get_value()
-                elif isinstance(input_fields[pref_id], ix.api.GuiLineEdit):
-                    value = input_fields[pref_id].get_text()
-                else:
-                    value = None
-                set_preference(key, value)
-            ok_button.disable()
-            apply_button.disable()
+class IdentifiedPushButton(ix.api.GuiPushButton):
+    def __init__(self, identifier, parent, *args, **kwargs):
+        super(IdentifiedPushButton, self).__init__(*args, **kwargs)
+        self.connect(self, 'EVT_ID_PUSH_BUTTON_CLICK',
+                     self.on_click)
+        self.identifier = identifier
+        self.parent = parent
 
-        def cancel(self, sender, evtid):
-            sender.get_window().hide()
+    def on_click(self, sender, evtid):
+        self.parent.button_click(self, evtid)
 
-        def enable_apply_buttons(self, sender, evtid):
-            ok_button.enable()
-            apply_button.enable()
 
-        def set_preference(self, sender, evtid, id):
-            if check_selection(ix.selection, (preferences[id]['kind'],)):
-                input_fields[id].set_text(str(ix.selection[0]))
-            self.enable_apply_buttons(sender, evtid)
+class PreferencesGui(ix.api.GuiWindow):
+    def __init__(self, title, x, y, w, h):
+        super(PreferencesGui, self).__init__(ix.application.get_event_window(), x, y, w, h)
+        self.preferences = preferences_list()
 
-        def clear_preference(self, sender, evtid, id):
-            if isinstance(input_fields[id], ix.api.GuiCheckbox):
-                input_fields[id].set_value(preferences_list()[id].get('default', False))
-            elif isinstance(input_fields[id], ix.api.GuiLineEdit):
-                input_fields[id].set_text(preferences_list()[id].get('default', ''))
-            ok_button.enable()
-            apply_button.enable()
+        vertical_spacing = 26
+        self.labels = []
+        self.input_fields = []
+        self.set_buttons = []
+        self.clear_buttons = []
+        self.set_title(title)
+        self.panel = ix.api.GuiPanel(self, 0, 0, self.get_width(), self.get_height())
+        self.panel.set_constraints(ix.api.GuiWidget.CONSTRAINT_LEFT, ix.api.GuiWidget.CONSTRAINT_TOP,
+                                   ix.api.GuiWidget.CONSTRAINT_RIGHT, ix.api.GuiWidget.CONSTRAINT_BOTTOM)
 
-        def buttonClick(self, sender, evtid):
-            if 'set_' in sender.identifier:
-                id = int(sender.identifier.replace('set_', ''))
-                self.set_preference(sender, evtid, id)
-            if 'clear' in sender.identifier:
-                id = int(sender.identifier.replace('clear_', ''))
-                self.clear_preference(sender, evtid, id)
+        # Form generation
+        ui_id = 1
+        pref_id = 0
+        for pref_id in range(len(self.preferences)):
+            key = self.preferences[pref_id]['key']
+            self.labels.append(ix.api.GuiLabel(self.panel, 10, vertical_spacing * ui_id + 4, 400, 22,
+                                          self.preferences[pref_id]['description']))
+            ui_id += 1
+            if self.preferences[pref_id]['kind'] == bool:
+                input_field = ix.api.GuiCheckbox(self.panel, 10, vertical_spacing * ui_id, "")
+                input_field.set_value(get_preference(key, self.preferences[pref_id].get('default', False)))
+                self.connect(input_field, 'EVT_ID_CHECKBOX_CLICK', self.enable_apply_buttons)
+            else:
+                input_field = ix.api.GuiLineEdit(self.panel, 10, vertical_spacing * ui_id, 340, 22)
+                input_field.set_text(get_preference(key, self.preferences[pref_id].get('default', '')))
+                self.connect(input_field, 'EVT_ID_LINE_EDIT_VALUE_EDITED', self.enable_apply_buttons)
+            self.input_fields.append(input_field)
+            if self.preferences[pref_id]['kind'] in ['ShadingLayer', 'OfContext']:
+                self.set_buttons.append(IdentifiedPushButton('set_{id}'.format(id=pref_id), self, self.panel, 360,
+                                                 vertical_spacing * ui_id, 100, 22, "Set to selected"))
+            self.clear_buttons.append(IdentifiedPushButton('clear_{id}'.format(id=pref_id), self, self.panel, 470,
+                                               vertical_spacing * ui_id, 60, 22, "Default"))
+            ui_id += 1
+        ui_id += 3
+        self.ok_button = ix.api.GuiPushButton(self.panel, 220, vertical_spacing * ui_id, 95, 22, "OK")
+        self.connect(self.ok_button, 'EVT_ID_PUSH_BUTTON_CLICK', self.ok)
+        self.ok_button.disable()
+        self.cancel_button = ix.api.GuiPushButton(self.panel, 320, vertical_spacing * ui_id, 95, 22, "Cancel")
+        self.connect(self.cancel_button, 'EVT_ID_PUSH_BUTTON_CLICK', self.cancel)
+        self.apply_button = ix.api.GuiPushButton(self.panel, 420, vertical_spacing * ui_id, 95, 22, "Apply")
+        self.connect(self.apply_button, 'EVT_ID_PUSH_BUTTON_CLICK', self.apply)
+        self.apply_button.disable()
 
-    class GuiPushButton(ix.api.GuiPushButton):
-        def __init__(self, identifier, *args, **kwargs):
-            ix.api.GuiPushButton.__init__(self, *args, **kwargs)
-            self.connect(self, 'EVT_ID_PUSH_BUTTON_CLICK',
-                         self.on_click)
-            self.identifier = identifier
+    def ok(self, sender, evtid):
+        self.apply(sender, evtid)
+        sender.get_window().hide()
 
-        def on_click(self, sender, evtid):
-            event_rewire.buttonClick(self, evtid)
+    def apply(self, sender, evtid):
+        for pref_id in range(len(self.preferences)):
+            key = self.preferences[pref_id]['key']
+            if isinstance(self.input_fields[pref_id], ix.api.GuiCheckbox):
+                value = self.input_fields[pref_id].get_value()
+            elif isinstance(self.input_fields[pref_id], ix.api.GuiLineEdit):
+                value = self.input_fields[pref_id].get_text()
+            else:
+                value = None
+            set_preference(key, value)
+        self.ok_button.disable()
+        self.apply_button.disable()
 
-    # Window creation
-    vertical_spacing = 26
-    labels = []
-    input_fields = []
-    set_buttons = []
-    clear_buttons = []
-    event_rewire = EventRewire()
+    def cancel(self, sender, evtid):
+        sender.get_window().hide()
 
-    clarisse_win = ix.application.get_event_window()
-    window = ix.api.GuiWindow(clarisse_win, 900, 450, 540, (len(preferences) + 4) * 44 + 10)
-    window.set_title('Clarisse Survival Kit Preferences')
+    def enable_apply_buttons(self, sender, evtid):
+        self.ok_button.enable()
+        self.apply_button.enable()
 
-    # Main widget creation
-    panel = ix.api.GuiPanel(window, 0, 0, window.get_width(), window.get_height())
-    panel.set_constraints(ix.api.GuiWidget.CONSTRAINT_LEFT, ix.api.GuiWidget.CONSTRAINT_TOP,
-                          ix.api.GuiWidget.CONSTRAINT_RIGHT, ix.api.GuiWidget.CONSTRAINT_BOTTOM)
+    def set_preference(self, sender, evtid, id):
+        if check_selection(ix.selection, (self.preferences[id]['kind'],)):
+            self.input_fields[id].set_text(str(ix.selection[0]))
+        self.enable_apply_buttons(sender, evtid)
 
-    # Form generation
-    ui_id = 1
-    pref_id = 0
-    for pref_id in range(len(preferences)):
-        key = preferences[pref_id]['key']
-        labels.append(
-            ix.api.GuiLabel(panel, 10, vertical_spacing * ui_id + 4, 400, 22, preferences[pref_id]['description']))
-        ui_id += 1
-        if preferences[pref_id]['kind'] == bool:
-            input_field = ix.api.GuiCheckbox(panel, 10, vertical_spacing * ui_id, "")
-            input_field.set_value(get_preference(key, preferences_list()[pref_id].get('default', False)))
-            event_rewire.connect(input_field, 'EVT_ID_CHECKBOX_CLICK',
-                                 event_rewire.enable_apply_buttons)
-        else:
-            input_field = ix.api.GuiLineEdit(panel, 10, vertical_spacing * ui_id, 340, 22)
-            input_field.set_text(get_preference(key, preferences_list()[pref_id].get('default', '')))
-            event_rewire.connect(input_field, 'EVT_ID_LINE_EDIT_VALUE_EDITED',
-                                 event_rewire.enable_apply_buttons)
-        input_fields.append(input_field)
-        if preferences[pref_id]['kind'] in ['ShadingLayer', 'OfContext']:
-            set_buttons.append(GuiPushButton('set_{id}'.format(id=pref_id), panel, 360,
-                                             vertical_spacing * ui_id, 100, 22, "Set to selected"))
-        clear_buttons.append(GuiPushButton('clear_{id}'.format(id=pref_id), panel, 470,
-                                           vertical_spacing * ui_id, 60, 22, "Default"))
-        ui_id += 1
-    ui_id += 3
-    ok_button = ix.api.GuiPushButton(panel, 220, vertical_spacing * ui_id, 95, 22, "OK")
-    event_rewire.connect(ok_button, 'EVT_ID_PUSH_BUTTON_CLICK', event_rewire.ok)
-    ok_button.disable()
-    cancel_button = ix.api.GuiPushButton(panel, 320, vertical_spacing * ui_id, 95, 22, "Cancel")
-    event_rewire.connect(cancel_button, 'EVT_ID_PUSH_BUTTON_CLICK', event_rewire.cancel)
-    apply_button = ix.api.GuiPushButton(panel, 420, vertical_spacing * ui_id, 95, 22, "Apply")
-    event_rewire.connect(apply_button, 'EVT_ID_PUSH_BUTTON_CLICK', event_rewire.apply)
-    apply_button.disable()
+    def clear_preference(self, sender, evtid, id):
+        if isinstance(self.input_fields[id], ix.api.GuiCheckbox):
+            self.input_fields[id].set_value(self.preferences[id].get('default', False))
+        elif isinstance(self.input_fields[id], ix.api.GuiLineEdit):
+            self.input_fields[id].set_text(self.preferences[id].get('default', ''))
+        self.ok_button.enable()
+        self.apply_button.enable()
 
-    window.show()
-    while window.is_shown():
-        ix.application.check_for_events()
-    window.destroy()
+    def button_click(self, sender, evtid):
+        if 'set_' in sender.identifier:
+            id = int(sender.identifier.replace('set_', ''))
+            self.set_preference(sender, evtid, id)
+        if 'clear' in sender.identifier:
+            id = int(sender.identifier.replace('clear_', ''))
+            self.clear_preference(sender, evtid, id)
 
 
 def set_preference(key, value, **kwargs):
@@ -176,4 +185,8 @@ def get_ix(ix_local):
 
 
 if __name__ == '__main__':
-    preferences_gui()
+    window = PreferencesGui('Clarisse Survival Kit Preferences', 900, 450, 540, (len(preferences_list()) + 4) * 44 + 10)
+    window.show()
+    while window.is_shown():
+        ix.application.check_for_events()
+    window.destroy()
